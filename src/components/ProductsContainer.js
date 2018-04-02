3bbfa7beb8bb29ac6f0dab2dbdbd27f2cda83425
Renan Zelaya <renan_zelaya@unitec.edu>
2018-04-01 18:45:16 -0600

    ya vota

--- src/components/ProductsContainer.js
+++ src/components/ProductsContainer.js
@@ -1,81 +1,138 @@
 import React, {Component} from 'react'
 import axios from 'axios'
 import Product from './Product'
-import update from 'immutability-helper'
 import ProductForm from './ProductForm'
+import update from 'immutability-helper'
 import '../semantic-dist/semantic.css'
 
-class ProductsContainer extends Component {
-	constructor(props) {
+class ProductsContainer extends Component{
+
+	constructor(props){
 		super(props)
 		this.state = {
 			products: [],
-			editingIdeaID: null
+			images: [],
+			editingProductId: null
 		}
 	}
 
-	componentDidMount() {
-		axios.get('http://localhost:3001/products.json')
-		.then(response => {
-			console.log(response)
-			this.setState({products: response.data})
-		})
-		.catch(error => {
-			console.log(error);
-		})
+	componentDidMount(){
+		axios.get('http://localhost:3001/products.json').then(
+			response => {
+				console.log(response)
+				this.setState({products: response.data})
+			}).catch(error => {
+				console.log(error)
+			})
 	}
 
+	handleProductUpVote = (productId) =>{
+		const nextProducts = this.state.products.map((product) => {
+		  if (product.id === productId) {
+			return Object.assign({}, product, {
+			  votes: product.votes + 1,
+			});
+		  } else {
+			return product;
+			}
+		  });
+		  this.setState({
+		  products: nextProducts,
+		});
+		}
+
 	addNewProduct = () => {
-		console.log('Boton presionado!')
 		axios.post(
-			'http://localhost:3001/products',
+			`http://localhost:3001/products/`,
 			{ product:
 				{
 					title: '',
-					product: ''
+					description: '',
+					productImageUrl: '',
+					submitterAvatarUrl: ''
 				}
 			}
 		).then(response => {
+			//agregar las nuevas ideas sin tener que refresh manualmente 
 			console.log(response)
-			const products = update(this.state.product, {
-				$splice: [[0, 0, response.data]]
+			const products = update(this.state.products, {
+				$splice: [[0,0,response.data]]
 			})
 			this.setState({
 				products: products,
-				editingProductID: response.data.id
+				editingProductId: response.data.id
 			})
-		}).catch(error => console.log(error))
+		}).catch(error => {
+			console.log(error)
+		})
 	}
 
 	updateProduct = (product) => {
 		const productIndex = this.state.products.findIndex(x => x.id === product.id)
-		const products = update(this.state.products, {
+		const products = update(this.state.products,{
 			[productIndex]: { $set: product }
 		})
-		this.setState({products: products})
+		this.setState({products: products, notification: 'Saved'})
 	}
+
+	reset = () => {
+		{this.setState({notification: ''})}
+	}
+
+	enableEditing = (id) => {
+		this.setState({editingProductId: id})
+	}
+
+	delete = (id) => {
+		axios.delete(
+			`http://localhost:3001/products/${id}`
+		).then(response => {
+			const productIndex = this.state.products.findIndex(x => x.id === id)
+			const products = update(this.state.products, {
+				$splice: [[productIndex, 1]]
+			})
+			this.setState({products: products})
+		}).catch(error => {
+			console.log(error)
+		})
+	}
+
 
 	render() {
-		return (
-			<div>
-				<div>
-					<button className="ui button" onClick={this.addNewProduct}>
+    const products = this.state.products.sort((a, b) => (
+      b.votes - a.votes
+      ));
+      const productComponents = products.map((product) => (
+        <Product
+          key={'product-' + product.id}
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          url={product.url}
+          votes={product.votes}
+          submitterAvatarUrl={product.submitterAvatarUrl}
+          productImageUrl={product.productImageUrl}
+					onVote={this.handleProductUpVote}
+					onClick={this.enableEditing}
+					onDelete={this.delete}
+        />
+    ));
+    return (
+      <div className='ui unstackable items'>
+				{productComponents}
+					<button className="newProductButton" onClick={this.addNewProduct}>
 						New Product
 					</button>
-				</div>
-				<div className='ui unstackable items'>
-					{this.state.products.map((product) => {
-						if(this.state.editingProductID == product.id) {
-							return(<ProductForm product={product} key={product.id} updateProduct={this.updateProduct}/>)
-						} else {
-							return(<Product product={product} key={product.id} />)
-						}
-					})}
-				</div>
-			</div>
-		);
-	}
-
+					<span className="notified">{this.state.notification}</span>
+						{this.state.products.map((product) => {
+							if(this.state.editingProductId === product.id){
+								return(<ProductForm product={product} key={product.id} updateProduct={this.updateProduct}
+								reset={this.reset}/>)
+							} 
+						})}
+      </div>
+    );
+  }
 }
 
 export default ProductsContainer
